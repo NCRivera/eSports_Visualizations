2f12eef5ca6c9b8bec64cdd61dc07b50c64943e9
Nicholas C. Rivera <riveranicholasc@gmail.com>
2021-03-21 12:23:37 -0400

    Modified Plot per Discussion

--- scripts/EsportsDataViz.R
+++ scripts/EsportsDataViz.R
@@ -7,27 +7,31 @@ OverwatchData <- read.csv(file = "data/overwatch_competitive_6_months.csv") %>%
 
 # OverwatchData %>% head()
 
-OverwatchData %>% 
+BarPlot <- OverwatchData %>% 
     ggplot(mapping = aes(x = hero, y = pick_rate_a, fill = hero_type)) + 
     geom_col(width = 0.75, color = "black") +
     facet_wrap(. ~ system) + 
     scale_fill_manual(values = c("orange", "red", "green", "blue")) +
+    scale_x_discrete(expand = c(0.01, 0.01)) +
     scale_y_continuous(labels = scales::percent) + 
     theme_minimal() +
     theme(axis.text.x = element_text(angle = 90)) + 
     labs(
-        title = "Overwatch Characters Pick Rate", 
+        title = "Overwatch Characters Pick Rate, Competitive Play", 
         subtitle = "Barchart: Percentage per System (6 Months)", 
         x = "Character",
         y = "Pick Rate Percentage",
-        fill = "Hero Type"
+        fill = "Hero Type", 
+        caption = "Source: https://www.overbuff.com/heroes"
     )
+BarPlot
 
-OverwatchData %>% 
+DotPlot <- OverwatchData %>% 
     ggplot(mapping = aes(x = win_rate, y = hero, fill = system)) + 
     geom_point(size = 2, shape = 21, color = "black")  +
     geom_segment(mapping = aes(yend = hero), xend = 0, color = "black") +
-    scale_y_discrete(limits = rev) +
+    scale_fill_manual(values = c("coral", "navy", "springgreen", "firebrick")) +
+    scale_y_discrete(limits = rev, expand = c(0.01, 0.01)) +
     scale_x_continuous(labels = scales::percent) + 
     facet_wrap(. ~ system) + 
     theme_minimal() + 
@@ -40,3 +44,4 @@ OverwatchData %>%
         y = "Hero", 
         fill = "System"
     )
+DotPlot
